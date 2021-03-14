28f7317a329efd97bdc41eb21ec7e572f6b6dd21
chaarke <54222322+chaarke@users.noreply.github.com>
2021-03-14 14:21:13 -0400

    Output formatting done. Just need responses for RoomInfo and ExamInfo.

--- src/reporting.java
+++ src/reporting.java
@@ -33,14 +33,7 @@ class Reporting {
         String username = args[0];
         String password = args[1];
 
-        try {
-
-            access.login(username, password);
-
-        } catch ( Exception e ) {
-            System.out.println("Failed to log in to Oracle. Please try again.");
-            return;
-        }
+        access.login(username, password);
 
         // Process the mode selection
 
@@ -55,19 +48,44 @@ class Reporting {
 
         switch (Integer.parseInt(args[2])) {
             case 1:
-                access.patientMode();
+                try {
+                    access.patientMode();
+                } catch (SQLException e) {
+                    System.out.println("SQL EXCEPTION: Failed to retrieve patient data.");
+                    e.printStackTrace();
+                    break;
+                }
                 break;
             case 2:
-                access.doctorMode();
+                try {
+                    access.doctorMode();
+                } catch (SQLException e) {
+                    System.out.println("SQL EXCEPTION: Failed to retrieve doctor data.");
+                    e.printStackTrace();
+                    break;
+                }
                 break;
             case 3:
-                access.admissionMode();
+                try {
+                    access.admissionMode();
+                } catch (SQLException e) {
+                    System.out.println("SQL EXCEPTION: Failed to retrieve admission data.");
+                    e.printStackTrace();
+                    break;
+                }
                 break;
             case 4:
-                access.updateAdmission();
+                try {
+                    access.updateAdmission();
+                } catch (SQLException e) {
+                    System.out.println("SQL EXCEPTION: Failed to update payment data.");
+                    e.printStackTrace();
+                    break;
+                }
                 break;
             default:
                 System.out.println("Invalid mode selected. Please enter a number 1-4.");
+                break;
         }
 
     }
@@ -75,37 +93,78 @@ class Reporting {
 
 class Access {
 
-    public void login(String username, String password) throws Exception {
+    DatabaseConnection db = new DatabaseConnection();
 
+    public void login(String username, String password) {
+        if (db.connect(username, password)) {
+            System.out.println("Welcome, " +  username);
+        } else {
+            System.out.println("Error signing on. Username or password is incorrect.");
+        }
     }
 
-    public void patientMode() {
+    public void patientMode() throws SQLException {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Patient SSN: ");
         String ssn =  input.nextLine();
-
+        ResultSet rs = db.getPatientInfo(ssn);
+        System.out.print("Patient SSN: " + rs.getString("ssn") + "\n"
+                        + "Patient First Name: " + rs.getString("fName") + "\n"
+                        + "Patient Last Name: " + rs.getString("lName") + "\n"
+                        + "Patient Address: " + rs.getString("address") + "\n");
+        rs.close();
+        db.wrapUp();
     }
 
-    public void doctorMode() {
+    public void doctorMode() throws SQLException {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Doctor ID: ");
         String id =  input.nextLine();
+        ResultSet rs = db.getDoctorInfo(id);
+        System.out.print("Doctor ID: " + rs.getString("id") + "\n"
+                + "Doctor First Name: " + rs.getString("fName") + "\n"
+                + "Doctor Last Name: " + rs.getString("lName") + "\n"
+                + "Doctor Gender: " + rs.getString("gender") + "\n"
+                + "Graduated From: " + rs.getString("graduatedFrom") + "\n"
+                + "Specialty: " + rs.getString("specialty") + "\n");
+        rs.close();
+        db.wrapUp();
 
     }
 
-    public void admissionMode() {
+    public void admissionMode() throws SQLException {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Admission Number: ");
         String num =  input.nextLine();
+        ResultSet rs = db.getAdmissionInfo(num);
+        System.out.print("Admission ID: " + rs.getString("AdmID") + "\n"
+                + "Patient SSN: " + rs.getString("SSN") + "\n"
+                + "Admission Date: " + rs.getString("admissionDate") + "\n"
+                + "Total Payment: " + rs.getString("payment") + "\n");
+        System.out.println("Rooms: ");
+        int x = 0;
+        while (x < 3) {
+            System.out.println ("room" + "\t" + "from date" + "\t" + "to date" + "\n");
+            x+=1;
+        }
+        System.out.println("Doctors who examined the patient in this admission: ");
+        x = 0;
+        while (x < 3) {
+            System.out.println("Doctor ID: " + "id");
+            x+=1;
+        }
+        rs.close();
+        db.wrapUp();
 
     }
 
-    public void updateAdmission() {
+    public void updateAdmission() throws SQLException {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Admission Number: ");
         String num =  input.nextLine();
         System.out.println("Enter new total payment: ");
-        String update = input.nextLine();
+        float update = input.nextFloat();
+        db.updatePayment(num, update);
 
     }
 }
@@ -160,7 +219,7 @@ class DatabaseConnection {
     public ResultSet getDoctorInfo(String docID) throws SQLException {
         try {
             if (doctorInfo != null) {
-                doctorInfo.setString(1, docID;
+                doctorInfo.setString(1, docID);
                 return doctorInfo.executeQuery();
             } else {
                 throw new Exception("No valid Connections open");
@@ -206,7 +265,7 @@ class DatabaseConnection {
         return null;
     }
 
-    public void WrapUp() throws SQLException {
+    public void wrapUp() throws SQLException {
         patientInfo.close();
         doctorInfo.close();
         roomInfo.close();
